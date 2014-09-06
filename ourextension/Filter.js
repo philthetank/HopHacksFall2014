117e302cae929324d3c0c123c136ccababbe4225
starkeyem <starkeyem@gmail.com>
2014-09-06 14:48:09 -0400

    changes from liz's laptop

--- ourextension/Filter.js
+++ ourextension/Filter.js
@@ -1,10 +1,19 @@
 function httpGet( theUrl )
 {
   var url = "https://www.readability.com/api/content/v1/parser?url=" + theUrl + "&token=8eb05a4320c1ad21786e8270ffbcbe0f80d9f711";
+//alert(url);
   var xhr = new XMLHttpRequest();
   xhr.open( "GET", url , false );
   xhr.send( null );
   return xhr.responseText;
 }
 
-window.onload = alert( httpGet( document.URL ) );
+chrome.tabs.getSelected( null, function(tab)
+{
+	var div = document.createElement("div");
+	div.innerHTML = httpGet( tab.url );
+	httpGet( tab.url );
+	document.body.appendChild( div );
+    });
+
+//window.onload = alert( httpGet( tablink ) );
