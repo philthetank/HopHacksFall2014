c03d76b0b798019216e9e416ae592bcab5dd9edc
Philip Seidel <philipseidel@me.com>
2014-09-06 15:53:11 -0400

    it's aliiiiiive

--- ourextension/Filter.js
+++ ourextension/Filter.js
@@ -1,7 +1,6 @@
 function httpGet( theUrl )
 {
   var url = "https://www.readability.com/api/content/v1/parser?url=" + theUrl + "&token=8eb05a4320c1ad21786e8270ffbcbe0f80d9f711";
-//alert(url);
   var xhr = new XMLHttpRequest();
   xhr.open( "GET", url , false );
   xhr.send( null );
@@ -11,9 +10,7 @@ function httpGet( theUrl )
 chrome.tabs.getSelected( null, function(tab)
 {
 	var div = document.createElement("div");
-	div.innerHTML = httpGet( tab.url );
-	httpGet( tab.url );
+	var json = JSON.parse( httpGet( tab.url ) );
+	div.innerHTML = json["content"];
 	document.body.appendChild( div );
-    });
-
-//window.onload = alert( httpGet( tablink ) );
+});
