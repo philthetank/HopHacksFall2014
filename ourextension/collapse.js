65e07fd58220749367c5fed8cd634b5b1c624fb0
Philip Seidel <philipseidel@me.com>
2014-09-07 04:20:46 -0400

    the stuff for making things work

--- ourextension/collapse.js
+++ ourextension/collapse.js
@@ -4,16 +4,20 @@
 	var index = 0;
 	var str = text.substr(index);
 	var mine = str;
-	var myreg = /\(.*?(,\W+|\d|\.).*?((\d|\.)[a-z]?)\)/g;
+//  	var myreg = /\((.*?\b)(\d|\.).*?((\d|\.)[a-z]?)\)/g;
+	var myreg = /(\(([^\)]*)(((\d{4}|([p][a][r][a]\.[[:space:]]\d+)|([p]\.[[:space:]]\d+))[a-z]?|\.))\))/g;
 
 	while((resultz = myreg.exec(str)) !== null){
-		var str = str.substr(index);
-		var sub = resultz[0];
-		var len = sub.length;
-		var yus = sub.substr(1, len-2);
-		index = myreg.lastindex;
-		var nnew = "<a title=\"" + yus + "\">(...)</a>" + " ";
-		mine = mine.replace(resultz[0], nnew);
+		//var innerReg = /(.*?)/;
+		//if ( innerReg.exec(resultz[0]) ) {	
+			var str = str.substr(index);
+			var sub = resultz[0];
+			var len = sub.length;
+			var yus = sub.substr(1, len-2);
+			index = myreg.lastindex;
+			var nnew = "<a title=\"" + yus + "\">(...)</a>" + " ";
+			mine = mine.replace(resultz[0], nnew);
+		//}
 	}
 
 	citationz.innerHTML = mine;
